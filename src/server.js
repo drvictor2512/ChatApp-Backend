92b727738e14e0626a114aa13d44e5710b020d50
Tran Thang <trvictor2512@gmail.com>
2026-04-02 20:23:38 +0700

    Thêm origin

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const bodyParser = express.json();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', "http://localhost:5174"],
+    origin: ['http://localhost:5173', "http://localhost:5174", "http://localhost:8081"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -39,7 +39,7 @@ connectDB().then(() => {
     const server = http.createServer(app)
     const io = new Server(server, {
         cors: {
-            origin: ['http://localhost:5173', "http://localhost:5174"],
+            origin: ['http://localhost:5173', "http://localhost:5174", "http://localhost:8081"],
             methods: ['GET', 'POST']
         }
     })
