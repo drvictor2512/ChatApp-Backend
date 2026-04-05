ede2341099433b60d7c11eb22fc0e0df0d539973
Tran Thang <trvictor2512@gmail.com>
2026-04-05 16:49:05 +0700

    Thêm origin

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const bodyParser = express.json();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', "http://localhost:5174", "http://localhost:8081"],
+    origin: ['http://localhost:5173', "http://localhost:5174", "http://localhost:8081", "https://zting-chat-web.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -39,7 +39,7 @@ connectDB().then(() => {
     const server = http.createServer(app)
     const io = new Server(server, {
         cors: {
-            origin: ['http://localhost:5173', "http://localhost:5174", "http://localhost:8081"],
+            origin: ['http://localhost:5173', "http://localhost:5174", "http://localhost:8081", "https://zting-chat-web.vercel.app"],
             methods: ['GET', 'POST']
         }
     })
